9250413bff2b8fee7616e6d999e9228bb8340c0c
AdrianR0driguez <007923676@coyote.csusb.edu>
2024-02-19 22:38:25 -0800

    Update: code is not running the way I want to, will try to solve

---
+++ Lab3_CF.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main(){
+    int x = 0;
+    int y = 5;
+
+    if (x < y){
+        printf("%i\n", x);
+        x = x + 3;
+    }else{
+        if(x % 2 == 0){
+            printf("x is even\n");
+        }else{
+            printf("x is odd\n");
+        }
+    }
+}
