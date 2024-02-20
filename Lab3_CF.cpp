150c60419c837d5da68a75e2dac676e240bc0a4f
AdrianR0driguez <007923676@coyote.csusb.edu>
2024-02-19 22:54:02 -0800

    Update: checking if it works if I push it again

--- Lab3_CF.cpp
+++ Lab3_CF.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int x = 0;
+    int x = 0; //tried changing value to 1 and still didn't update after saving and compiling and yes I did save
     int y = 5;
 
     if (x < y){
